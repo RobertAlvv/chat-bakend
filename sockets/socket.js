e276cc5f84b8d2d451a8cdc2cf1affcf91f5f7c7
root <root@ROBERT-ALVAREZ.localdomain>
2021-03-29 13:38:05 -0400

    fin seccion 10

---
+++ sockets/socket.js
@@ -0,0 +1,38 @@
+const { comprobarJWT } = require('../helpers/jwt');
+const { io } = require('../index');
+const {usuarioConectado, usuarioDesconectado, mensajeGuardado} = require('../controllers/socket'); 
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    if(!valido){
+        return client.disconnect();
+    }
+
+    usuarioConectado(uid);
+
+    client.join(uid);
+
+    client.on('mensaje-personal', async (payload) => {
+
+        await mensajeGuardado(payload);
+
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
+
+    client.on('disconnect', () => {
+        usuarioDesconectado(uid);
+    });
+
+
+    
+    // client.on('mensaje', ( payload ) => {
+    //     console.log('Mensaje', payload);
+    //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    // });
+
+
+});
